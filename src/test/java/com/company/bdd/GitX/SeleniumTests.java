b2a56ab09827a07cec7ec79bbb1c401c49e4254f
AnushreePandey <anushree.pandey88@gmail.com>
2022-05-15 17:55:13 +0530

    first commit of 2nd Project

---
+++ src/test/java/com/company/bdd/GitX/SeleniumTests.java
@@ -0,0 +1,33 @@
+package com.company.bdd.GitX;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTests {
+
+	
+	@Test
+	public void TestLoginSel() {
+		System.out.println("TestLoginSel");
+	}
+
+	@Test
+	public void TestHomeSel() {
+		System.out.println("TestHomeSel");
+	}
+
+	@Test
+	public void TestMachinesSel() {
+		System.out.println("TestMachinesSel");
+	}
+
+	@Test
+	public void TestBatchesSel() {
+		System.out.println("TestBatchesSel");
+	}
+
+	@Test
+	public void TestReportsSel() {
+		System.out.println("TestReportsSel");
+	}
+
+}
